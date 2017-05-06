b697ee743628f02f60678f37e94191d21b3d2035
Legiani <kuba.bednar@seznam.cz>
2017-05-06 17:37:38 +0200

    Arduino_test3

---
+++ JunglePlay/JunglePlayPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace JunglePlay
+{
+	public partial class JunglePlayPage : ContentPage
+	{
+		public JunglePlayPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ iOS/AudioService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using AVFoundation;
+using Foundation;
+using JunglePlay.iOS;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AudioService))]
+namespace JunglePlay.iOS
+{
+	public class AudioService : IAudio
+	{
+		public void PlayAudioFile(string fileName)
+		{
+			string sFilePath = NSBundle.MainBundle.PathForResource(Path.GetFileNameWithoutExtension(fileName), Path.GetExtension(fileName));
+			var url = NSUrl.FromString(sFilePath);
+			var _player = AVAudioPlayer.FromUrl(url);
+			_player.FinishedPlaying += (object sender, AVStatusEventArgs e) =>
+			{
+				_player = null;
+			};
+			_player.Play();
+		}
+	}
+}
